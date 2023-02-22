455c0e9b2197c6946a6fe6f6a08f6a049d953d11
Teodor.Popa <teodor.popa@mrm.com>
2023-02-22 18:21:06 +0200

    Chapter 6.2 & Chapter 6.3

---
+++ c_62_match_control_flow/src/main.rs
@@ -0,0 +1,46 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alabama)); 
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Luxky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter for {:?}!", state);
+            25
+        },
+        _ => {
+            println!("Not found");
+            0
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
