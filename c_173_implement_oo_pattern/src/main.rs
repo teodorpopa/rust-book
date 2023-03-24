1da27e53d805facf411954a1cac54417fd069079
Teodor.Popa <teodor.popa@mrm.com>
2023-03-24 12:10:34 +0200

    Chapter 17

---
+++ c_173_implement_oo_pattern/src/main.rs
@@ -0,0 +1,14 @@
+use c_173_implement_oo_pattern::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
+
